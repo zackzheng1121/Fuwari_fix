c346eb0a0354de50923ab4e30b7c70481ebcad15
zackzheng1121 <86220968+zackzheng1121@users.noreply.github.com>
2026-05-28 17:38:17 +0800

    choose some thing

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://pg72.tw/",
+	site: "https://blog-test.zackzheng.me",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
